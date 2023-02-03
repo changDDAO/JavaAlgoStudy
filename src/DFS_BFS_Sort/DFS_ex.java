2b71642f64b44b144a8e37a42fb80c71f3bcf599
Changhogod <younch8342@naver.com>
2023-02-03 15:48:34 +0900

    dfs practice3

---
+++ src/DFS_BFS_Sort/DFS_ex.java
@@ -0,0 +1,9 @@
+package DFS_BFS_Sort;
+
+public class DFS_ex {
+    public static boolean [] visited = new boolean[9];
+
+    public static void main(String[] args) {
+        System.out.println(visited[0]);
+    }
+}
