49f9a7e472494f10ecb27b6ecbf32eaef03faad8
Changhogod <younch8342@naver.com>
2023-02-04 21:29:08 +0900

    InsertionSort 갑자기 떠올라서 구현해봄(changho)

--- src/BinarySearch_DynamicProgramming/InsertionSort.java
+++ src/BinarySearch_DynamicProgramming/InsertionSort.java
@@ -12,7 +12,6 @@ public class InsertionSort {
                 }else break;
             }
         }
-        for(int i=0;i<arr.length;i++)
-            System.out.println(arr[i]);
+        for (int j : arr) System.out.println(j);
     }
 }
